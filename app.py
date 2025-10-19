79002c9883a9c385165a6e78af499ebfabaced8b
“[padeghs]” <[padeghs@gmail.com]>
2025-10-19 22:15:37 +0700

    flask

--- app.py
+++ app.py
@@ -6,4 +6,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def indexpage():
-   return render_template("index.html")
+   return render_template("index.html" , value="1234")
+
+
+
+
+if __name__ == "__main__":
+   app.run( host='0.0.0.0' , debug=True, port=80 )
+
+
+
